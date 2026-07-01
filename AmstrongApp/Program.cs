afca99f872bb6d6ce0404d9bd4d3b280ea2d2cdc
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-06-30 23:11:57 -0700

    new code are being secure

---
+++ AmstrongApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number, remainder, result = 0, originalNumber;
+
+        Console.Write("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        originalNumber = number;
+
+        while (number != 0)
+        {
+            remainder = number % 10;
+            result = result + (remainder * remainder * remainder);
+            number = number / 10;
+        }
+
+        if (result == originalNumber)
+        {
+            Console.WriteLine(originalNumber + " is an Armstrong Number.");
+        }
+        else
+        {
+            Console.WriteLine(originalNumber + " is not an Armstrong Number.");
+        }
+    }
+}
---
+++ FactorialApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int fact = 1;
+
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            fact *= i;
+        }
+
+        Console.WriteLine("Factorial = " + fact);
+    }
+}
