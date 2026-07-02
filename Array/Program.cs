f77f71e780ee3cce2993f512bdc48c24e617fa0e
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-01 20:39:59 -0700

    the array point are noticed

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] numbers = new int[n];
+        int sum = 0;
+
+        Console.WriteLine("Enter the elements:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Element " + (i + 1) + ": ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            sum = sum + numbers[i];
+        }
+
+        Console.WriteLine("Sum of the array elements = " + sum);
+    }
+}
